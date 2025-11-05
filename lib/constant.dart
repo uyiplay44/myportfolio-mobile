3611d2c9eb78efba6bdd96589bc326617d70b96a
uyiplay44 <uyiplay44@hotmail.com>
2025-11-05 12:06:16 +0100

    colorarrangement

--- lib/constant.dart
+++ lib/constant.dart
@@ -4,6 +4,10 @@ const Color primaryColor = Color(0XFF23570d);
 
 const Color bgColor = Color(0XFF68b99d);
 
+const Color buttonColor = Color(0XFF363795);
+
+const Color actionButtonColor = Color(0XFFe53935);
+
 const double defaultPadding = 16.5;
 
 const double defaultBorderRadius = 12.0;
